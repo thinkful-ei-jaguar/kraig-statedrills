19d167eb5974e8f6b71212a14383518661b1e48f
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-04-09 06:46:16 -0400

    upload

--- kraig-state-drills/src/Tabs.js
+++ kraig-state-drills/src/Tabs.js
@@ -1,9 +1,47 @@
 import React from "react";
 
 class Tabs extends React.Component {
-  render() {
-    return <div />
+    static defaultProps = { tabs: [] };
+state = {
+    currentTabIndex: 0
+  };
+
+  handleButtonClick = (index) => {
+    this.setState({ currentTabIndex: index })
+    console.log('button clicked!',{ index });
   }
-}
+
+
+    renderButtons() {
+      return this.props.tabs.map((tab, index) => (
+        <button key={index} onClick={() => this.handleButtonClick}>
+          {tab.name}
+        </button>
+      ))
+    }
+    renderContent() {
+      const currentTab = this.props.tabs[this.state.currentTabIndex]
+      return (
+        <div className='content'>
+          {currentTab.content}
+        </div>
+      )
+    }  
+
+
+    render() {
+        
+      return (
+        <div>
+          {this.renderButtons()}
+        {!!this.props.tabs.length && this.renderContent()}
+          </div>)
+          
+
+
+        
+      
+    }
+  }
 
 export default Tabs;
